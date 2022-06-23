e893a9c4d3ce6e29875d85bdd0f90dd4d10c5a4a
= <=>
2022-06-22 21:37:01 -0400

    url manupulation

--- fetch.js
+++ fetch.js
@@ -5,13 +5,15 @@ const post = async (url) => {
 method:'GET',
 headers: { 
     'Content-Type': 'application/json',
+    // 'Access-Control-Allow-Headers'
+    // 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Authorization, Content-Language, Accept, Accept-Language' always;
 },
 });
     const jsonResponse = await fetchRequest.json();
     return jsonResponse
 };
 
-post('http://api.weatherapi.com/v1/forecast.json?key=e76d66218c9a4eda932221659220706&q=clifton&days=7')
+post('http://api.weatherapi.com/v1/forecast.json?key=e76d66218c9a4eda932221659220706&q=clifton&days=7/')
 .then( (jsonData)=>{ processData(jsonData)
 })    
 .catch ((error)=>{
